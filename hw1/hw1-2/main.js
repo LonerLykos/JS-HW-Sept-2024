e937619e349375c44787ee1efce1ac5367fa8c65
LonerLykos <loner.lykos.project@gmail.com>
2024-11-03 12:54:10 +0200

    init commit

---
+++ hw1/hw1-2/main.js
@@ -0,0 +1,7 @@
+let firstName = 'Yurii';
+let middleName = 'Ihorovich';
+let lastName = 'Kondrashov';
+
+let person = `${firstName} ${middleName} ${lastName}` ;
+// let person = firstName + ' ' + middleName + ' ' + lastName ;
+ console.log(person);
---
+++ hw1/hw1-4/main.js
@@ -0,0 +1,7 @@
+let firstName = prompt('Your firstName');
+let middleName = prompt('Your middleName');
+let lastName = prompt('Your lastName');
+
+let person = `${firstName} ${middleName} ${lastName}`;
+
+console.log(person);
